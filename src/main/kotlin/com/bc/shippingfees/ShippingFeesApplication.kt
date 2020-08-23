ae236ae31ce4e8cc7122132c5a635c0d7c17915a
unknown <torres@alunos.utfpr.edu.br>
2020-08-23 19:40:49 -0300

    swagger trial - not working

--- src/main/kotlin/com/bc/shippingfees/ShippingFeesApplication.kt
+++ src/main/kotlin/com/bc/shippingfees/ShippingFeesApplication.kt
@@ -1,14 +1,10 @@
 package com.bc.shippingfees
 
-import Models.Product
-import com.bc.shippingfees.mocks.ProductsMock
-import net.minidev.json.JSONObject
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
 
 @SpringBootApplication
-class ShippingFeesApplication
+class ShippingFeesApplication{}
 
 fun main(args: Array<String>) {
     runApplication<ShippingFeesApplication>(*args)
---
+++ src/main/kotlin/com/bc/shippingfees/configurations/Swagger.kt
@@ -0,0 +1,46 @@
+package com.bc.shippingfees.configurations
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+
+@Configuration
+@EnableSwagger2
+class SwaggerConfig : WebMvcConfigurationSupport() {
+
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/")
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/")
+    }
+
+    @Bean
+    fun productApi(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.bc"))
+                .build()
+                .apiInfo(metaData())
+    }
+
+    private fun metaData(): ApiInfo {
+        return ApiInfoBuilder()
+                .title("Spring Boot REST API")
+                .description("Spring Boot REST API")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .build()
+    }
+
+}
+
