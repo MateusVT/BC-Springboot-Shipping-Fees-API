2581ed9138d153c73827895cf16c1db7752bf6b7
unknown <torres@alunos.utfpr.edu.br>
2020-08-23 00:44:33 -0300

    replacing infinity by MAX_VALUE

--- src/main/kotlin/com/bc/shippingfees/mock/FeeRanges.kt
+++ src/main/kotlin/com/bc/shippingfees/mock/FeeRanges.kt
@@ -1,14 +1,12 @@
 package com.bc.shippingfees.mock
-
 import Models.FeesRange
-import jdk.nashorn.internal.objects.Global.Infinity
 
 object FeeRangesMock {
     var feesRanges = listOf(
-            FeesRange(0, (0.0..Infinity), 10.0, 0.05),
-            FeesRange(1, (0.0..Infinity), 4.3, 0.12),
+            FeesRange(0, (0.0..Double.MAX_VALUE), 10.0, 0.05),
+            FeesRange(1, (0.0..Double.MAX_VALUE), 4.3, 0.12),
             FeesRange(2, (0.0..5.0), 2.10, 1.10),
-            FeesRange(2, (5.0..Infinity), 10.0, 0.01)
+            FeesRange(2, (5.0..Double.MAX_VALUE), 10.0, 0.01)
     )
 }
 
