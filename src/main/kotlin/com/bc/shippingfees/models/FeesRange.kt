2deee8f22706551dd87dba2001aa93b231c3bb5f
unknown <torres@alunos.utfpr.edu.br>
2020-08-24 03:44:43 -0300

    finished

--- src/main/kotlin/com/bc/shippingfees/models/FeesRange.kt
+++ src/main/kotlin/com/bc/shippingfees/models/FeesRange.kt
@@ -1,7 +1,5 @@
 package Models
 
-import javafx.scene.text.FontWeight
-
 data class FeesRange(
         val idCompany: Long,
         val rangeWeight: ClosedFloatingPointRange<Double>,
--- src/main/kotlin/com/bc/shippingfees/utils/Calcute.kt
+++ src/main/kotlin/com/bc/shippingfees/utils/Calcute.kt
@@ -2,7 +2,6 @@ package com.bc.shippingfees.utils
 
 import Models.Product
 
-
 fun calculateFee(fixedFee: Double, kmByKgFee: Double, product: Product, distance: Long): Double {
     return fixedFee + (product.weight * distance * kmByKgFee)
 }
